3c64c400e9dfe3a38f99771b8606710ea93cb868
lighthenry <daniel.holman99@gmail.com>
2023-07-04 00:21:51 +0100

    Added drop cap to posts - JS file adds class to <p>s generated by haskell

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 //hello
 //this file needs to write.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const posts = document.querySelectorAll('.post-content'); 
+    posts.forEach(post => {
+        const firstParagraph = post.querySelector('p');
+        if(firstParagraph) {
+            firstParagraph.classList.add('drop-cap-jerusalem');
+        }
+    });
+});
