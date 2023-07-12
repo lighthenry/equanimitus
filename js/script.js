07a0302b700aaa543d77e609d8c4226db6a8dd9b
lighthenry <daniel.holman99@gmail.com>
2023-07-12 12:03:47 +0100

    Fixed drop cap on Portfolio page by updating the javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //hello
 //this file needs to write.
 
-document.addEventListener("DOMContentLoaded", function() {
-    const posts = document.querySelectorAll('.post-content'); 
-    posts.forEach(post => {
-        const firstParagraph = post.querySelector('p');
-        if(firstParagraph) {
-            firstParagraph.classList.add('drop-cap-jerusalem');
+document.addEventListener("DOMContentLoaded", function () {
+    var posts = document.getElementsByClassName('post-content');
+    for (var i = 0; i < posts.length; i++) {
+        var paragraphs = posts[i].getElementsByTagName('p');
+        if (paragraphs.length > 0) {
+            paragraphs[0].className += ' drop-cap-jerusalem';
         }
-    });
+    }
 });
